ac6b94cd00a425270b06eabd638867bd6f706615
Mikalai Kliuyeu <nickolay.klyuev@gmail.com>
2025-08-26 12:59:01 +0300

    add logic with no args specified

--- main.cpp
+++ main.cpp
@@ -2,16 +2,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <regex>
 #include <filesystem>
 #include <cstdlib>
 
 #define SWAY_CONFIG_FILE_PATH "/.config/sway/config"
+#define CURRENT_DIR_PATH "."
 #define BACKGROUND_REGEX R"(^output\s\*\sbg\s)"
+#define IMAGE_REGEX R"(\.(jpg|png|jpeg)$)"
 
-namespace sway {
+namespace utility {
     std::string make_wallpaper_line(const std::string& path) {
         return "output * bg " + path + " fill";
+    }
+
+    std::string make_image_path(const std::string& image_filename) {
+        return std::filesystem::current_path().string() + '/' + image_filename;
     }
 }
 
@@ -19,24 +26,30 @@ namespace check {
     unsigned int err_count = 0;
     std::vector<std::string> err_messages;
 
+    #define PUSH_ERR(message) err_messages.push_back(message); ++err_count;
+
     void file_path(const std::string& path, const std::string& message_on_fail) {
         if (!std::filesystem::exists(path)) {
-            err_messages.push_back(message_on_fail);
-            ++err_count;
+            PUSH_ERR(message_on_fail)
         }
     }
 
     void wallpaper_line_index(int i) {
         if (i == -1) {
-            err_messages.push_back("Wallpaper config line haven't been found");
-            ++err_count;
+            PUSH_ERR("Wallpaper config line haven't been found")
         }
     }
 
-    void arguments(int argc) {
-        if (argc != 2) {
-            err_messages.push_back("Programm allows 1 argument with an image's path");
-            ++err_count;
+    void image_filename(const std::string& name) {
+        std::regex name_pattern(IMAGE_REGEX);
+        if (!std::regex_search(name, name_pattern)) {
+            PUSH_ERR("Specified file is NOT an image. (Supported formats: png, jpg, jpeg)");
+        }
+    }
+
+    void images(const std::unordered_map<int, std::string>& map) {
+        if (map.empty()) {
+            PUSH_ERR("No images found in the current dir")
         }
     }
 
@@ -56,18 +69,61 @@ namespace check {
 #define CHECK_RESULT() if (check::result() == EXIT_FAILURE) { return EXIT_FAILURE; }
 
 int main (int argc, char *argv[]) {
-
-    check::arguments(argc);
-
-    CHECK_RESULT()
-
     const std::string home = std::getenv("HOME");
     const std::string sway_config_path = home + SWAY_CONFIG_FILE_PATH;
-    const std::string image_path = std::filesystem::current_path().string() + '/' + argv[1];
 
     check::file_path(sway_config_path, "Sway config file doesn't exist!");
-    check::file_path(image_path, "Image's path is not valid: " + image_path);
+    CHECK_RESULT()
 
+    std::string image_path;
+
+    if (argc == 1) {
+        // No any image's path specified, trying to check current user location for images
+        std::regex image_pattern(IMAGE_REGEX);
+        std::unordered_map<int, std::string> found_images;
+
+        int index = 1;
+        for (const auto& file : std::filesystem::directory_iterator(CURRENT_DIR_PATH)) {
+            const std::string file_name = file.path().filename().string();
+
+            if (std::regex_search(file_name, image_pattern)) {
+                found_images[index] = file_name;
+                std::cout << index << ": " + file_name << '\n';
+                ++index;
+            }
+        }
+
+        check::images(found_images);
+        CHECK_RESULT()
+        
+        std::cout << "0: Cancel" << '\n';
+
+    choose_image:
+        std::cout << '\n';
+        std::cout << "Which image set on wall? (number):" << '\n';
+        std::cin >> index;
+
+        if (index == 0) {
+            std::cout << "Canceled..." << '\n';
+            return EXIT_FAILURE;
+        }
+
+        if (!found_images.contains(index)) {
+            std::cout << "Specified number is NOT valid" << '\n';
+            goto choose_image;
+        }
+
+        image_path = utility::make_image_path(found_images.at(index));
+    }
+    else if (argc >= 2) {
+        // Image name specified
+        check::image_filename(argv[1]);
+        CHECK_RESULT()
+
+        image_path = utility::make_image_path(argv[1]);
+    }
+
+    check::file_path(image_path, "Image's path is not valid: " + image_path);
     CHECK_RESULT()
 
     std::ifstream sway_config_file(sway_config_path);
@@ -82,7 +138,7 @@ int main (int argc, char *argv[]) {
     std::regex bg_pattern(BACKGROUND_REGEX);
     
     int wallpaper_line = -1;
-    for (int i = 0; i < sway_config_lines.size(); ++i) {
+    for (size_t i = 0; i < sway_config_lines.size(); ++i) {
         if (std::regex_search(sway_config_lines[i], bg_pattern)) {
             wallpaper_line = i;
             break;
@@ -93,11 +149,11 @@ int main (int argc, char *argv[]) {
     
     CHECK_RESULT()
 
-    temp_line = sway::make_wallpaper_line(image_path);
+    temp_line = utility::make_wallpaper_line(image_path);
     
     if (temp_line == sway_config_lines[wallpaper_line]) {
         std::cout << "Wallpaper is the same. Nothing has been changed" << '\n';
-        return EXIT_SUCCESS;
+        return EXIT_FAILURE;
     }
 
     sway_config_lines[wallpaper_line] = temp_line;
@@ -110,7 +166,7 @@ int main (int argc, char *argv[]) {
         out_sway_config << line << '\n';
     }
 
-    std::cout << "Wallpaper has been updated" << '\n';
+    std::cout << "Wallpaper has been updated in sway's config file. Please reload sway. (command: swaymsg reload)" << '\n';
 
     return EXIT_SUCCESS;
 }
